4bc813c7de80e80bba007bbeb7c584fe8cbc03c8
Avi6943 <105759571+Avi6943@users.noreply.github.com>
2022-08-26 14:38:16 -0400

    Add files via upload

---
+++ student.js
@@ -0,0 +1,21 @@
+
+class Student
+{
+constructor(name,age,grade)
+{
+this.name=name;
+this.age=age;
+this.grade=grade;
+}
+show()
+{
+  console.log(this.name);
+  console.log(this.age);
+  console.log(this.grade);
+}
+
+   
+}
+
+
+
